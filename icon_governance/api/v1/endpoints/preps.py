a8d3908882f4eb032ef3ead198c8cee0997b611f
robcxyz <rob@geometrylabs.io>
2021-10-14 13:08:02 -0700

    feat: add prep details endpoint

--- icon_governance/api/v1/endpoints/preps.py
+++ icon_governance/api/v1/endpoints/preps.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlmodel import select
@@ -18,4 +18,20 @@ async def get_preps(
     """Return list of preps"""
     result = await session.execute(select(Prep).order_by(Prep.delegated))
     preps = result.scalars().all()
+
+    return preps
+
+
+@router.get("/preps/{address}")
+async def get_prep(
+    address: str,
+    session: AsyncSession = Depends(get_session),
+) -> List[Prep]:
+    """Return a single prep."""
+    result = await session.execute(select(Prep).where(Prep.address == address))
+    preps = result.scalars().all()
+
+    if len(preps) == 0:
+        raise HTTPException(status_code=204, detail="P-Rep address not found")
+
     return preps
--- tests/integration/api/test_preps.py
+++ tests/integration/api/test_preps.py
@@ -7,3 +7,15 @@ from icon_governance.config import settings
 def test_api_get_preps(db: Session, client: TestClient):
     response = client.get(f"{settings.REST_PREFIX}/preps")
     assert response.status_code == 200
+
+
+def test_api_get_prep(db: Session, client: TestClient):
+    response = client.get(
+        f"{settings.REST_PREFIX}/preps/hx0b047c751658f7ce1b2595da34d57a0e7dad357d"
+    )
+    assert response.status_code == 200
+
+
+def test_api_get_prep_error(db: Session, client: TestClient):
+    response = client.get(f"{settings.REST_PREFIX}/preps/hx0b047c751658f7ce1b2595da34d57a0")
+    assert response.status_code == 204
