4908c32ae6ee05544564670cca6eb5a17d1a655b
robcxyz <rob@geometrylabs.io>
2021-11-02 21:14:04 -0700

    chore: trim db

--- icon_governance/db.py
+++ icon_governance/db.py
@@ -35,23 +35,6 @@ async def get_session() -> AsyncSession:
         yield session
 
 
-from contextlib import contextmanager
-
-
-@contextmanager
-def session_scope():
-    """Provide a transactional scope around a series of operations."""
-    session = Session()
-    try:
-        yield session
-        session.commit()
-    except:
-        session.rollback()
-        raise
-    finally:
-        session.close()
-
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 session_factory = sessionmaker(bind=engine)
 # session = SessionMade()
