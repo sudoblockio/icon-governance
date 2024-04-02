a890e1730840725c44a4ae36e70f815a4ee64a66
robcxyz <rob@sudoblock.io>
2024-04-02 11:23:18 -0700

    fix: make apy column nullable

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 [//]: # (![Uptime]&#40;https://img.shields.io/endpoint?url=https%3A%2F%2Fraw.githubusercontent.com%2Fgeometry-labs%2Ficon-status-page%2Fmaster%2Fapi%2Fdev-governance-service%2Fuptime.json&#41;)
 
 [Live API Docs](https://tracker.icon.community/api/v1/governance/docs)
+
 Off chain indexer for the ICON Blockchain serving the **governance** context of the [icon-explorer](https://github.com/geometry-labs/icon-explorer). Service is broken up into API and worker components that are run as individual docker containers. It depends on data coming in from [icon-etl](https://github.com/geometry-labs/icon-etl) over a Kafka message queue with persistence on a postgres database.
 
 ### Deployment
--- icon_governance/models/apy_time.py
+++ icon_governance/models/apy_time.py
@@ -6,27 +6,27 @@ from sqlmodel import Field, SQLModel
 
 class ApyTime(SQLModel, table=True):
     timestamp: int = Field(primary_key=True)
-    height: int = Field(nullable=False)
+    height: int = Field(nullable=True)
 
-    i_global: float = Field(nullable=False)
-    i_prep: float = Field(nullable=False)
-    i_cps: float = Field(nullable=False)
-    i_relay: float = Field(nullable=False)
+    i_global: float = Field(nullable=True)
+    i_prep: float = Field(nullable=True)
+    i_cps: float = Field(nullable=True)
+    i_relay: float = Field(nullable=True)
 
     # Pre / post iiss 4.0
     i_voter: Optional[float] = Field(nullable=True)
     i_wage: Optional[float] = Field(nullable=True)
 
-    staking_apy: float = Field(nullable=False)
-    prep_apy: float = Field(nullable=False)
+    staking_apy: float = Field(nullable=True)
+    prep_apy: float = Field(nullable=True)
 
-    total_delegated: float = Field(nullable=False)
-    total_stake: float = Field(nullable=False)
-    total_bonded: float = Field(nullable=False)
-    total_power: float = Field(nullable=False)
+    total_delegated: float = Field(nullable=True)
+    total_stake: float = Field(nullable=True)
+    total_bonded: float = Field(nullable=True)
+    total_power: float = Field(nullable=True)
 
-    total_wage: float = Field(nullable=False)
-    active_preps: int = Field(nullable=False)
+    total_wage: float = Field(nullable=True)
+    active_preps: int = Field(nullable=True)
 
 
     @declared_attr
