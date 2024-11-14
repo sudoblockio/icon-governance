8a2b0bc69cff04a326cc18b0ae2930dbdead6f31
Rob Cannon <rob@sudoblock.io>
2024-11-14 18:14:33 +0700

    fix: rm echo in db and add pool

--- icon_governance/db.py
+++ icon_governance/db.py
@@ -22,7 +22,13 @@ SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2" + SQLALCHEMY_DATABASE_URL_STUB
 
 logger.info(f"Connecting to server: {settings.POSTGRES_SERVER} and {settings.POSTGRES_DATABASE}")
 
-async_engine = create_async_engine(ASYNC_SQLALCHEMY_DATABASE_URL, echo=True, future=True)
+async_engine = create_async_engine(
+    ASYNC_SQLALCHEMY_DATABASE_URL,
+    echo=False,
+    future=True,
+    pool_size=10,
+    max_overflow=5,
+)
 
 
 # Run onetime if we want to init with a prebuilt table of attributes
@@ -32,14 +38,14 @@ async def init_db():
 
 
 async def get_session() -> AsyncSession:
-    async_session = sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
+    async_session = sessionmaker(
+        async_engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+    )
     async with async_session() as session:
         yield session
 
 
-try:
-    engine = create_engine(SQLALCHEMY_DATABASE_URL)
-    session_factory = sessionmaker(bind=engine)
-except Exception as e:
-    logger.error(f"Database connection dropped: {str(e)}")
-    raise e
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+session_factory = sessionmaker(bind=engine)
